35802389d6f65990e28748a302edb2073d45b507
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-20 09:10:27 +0200

    adds js to hide left sidebar on click

--- assets 2/js/index.js
+++ assets 2/js/index.js
@@ -4,3 +4,10 @@ const nascondiAnnunciBtn = document.getElementById("nascondiAnnunciBtn");
 nascondiAnnunciBtn.addEventListener("click", () => {
   annunci.remove();
 });
+
+const activities = document.getElementById("activities");
+const hideActivitiesBtn = document.getElementById("hideActivitiesBtn");
+
+hideActivitiesBtn.addEventListener("click", () => {
+  activities.classList.remove("d-lg-block");
+});
