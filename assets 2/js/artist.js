dc7b95a9ccec7f54cff3c647fa6498c10db79e94
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-06-20 17:42:14 +0200

    update artist

--- assets 2/js/artist.js
+++ assets 2/js/artist.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     var query = "Empire of the sun";
     var endpoint = "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + query;
@@ -10,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
     function getRandomTrackPlays() {
         return Math.floor(Math.random() * (2000000 - 100000 + 1)) + 100000;
     }
-
 
     fetch(endpoint)
         .then(function(response) {
@@ -34,8 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.getElementById('artist-name').textContent = artist.name;
                 document.getElementById('monthly-listeners').textContent = getRandomListeners().toLocaleString() + " ascoltatori mensili";
 
-
-                // Imposta la "picture_big" dell'artista come sfondo dell'header
                 var artistHeader = document.getElementById('artist-header');
                 artistHeader.style.backgroundImage = "url(" + artist.picture_big + ")";
 
@@ -61,30 +57,82 @@ document.addEventListener("DOMContentLoaded", function() {
 
                     trackItem.addEventListener('click', function() {
                         if (currentTrack === track) {
-                            // Se il brano è lo stesso, ferma la riproduzione
-                        if (!audioPlayer.paused) {
-                            audioPlayer.pause();
-                            playPauseButton.innerHTML = getPlayIcon();  // Cambia icona a play
-                         } else {
-                            audioPlayer.play();
-                            playPauseButton.innerHTML = getPauseIcon();  // Cambia icona a pausa
+                            if (!audioPlayer.paused) {
+                                audioPlayer.pause();
+                                playPauseButton.innerHTML = getPlayIcon();
+                            } else {
+                                audioPlayer.play();
+                                playPauseButton.innerHTML = getPauseIcon();
                             }
                         } else {
                             currentTrack = track;
                             updatePlayer(track);
-                         }
-                        });
+                        }
+                    });
 
                     tracksList.appendChild(trackItem);
+                });
+
+                playPauseButton.addEventListener('click', function() {
+                    if (currentTrack) {
+                        if (audioPlayer.paused) {
+                            audioPlayer.play();
+                            playPauseButton.innerHTML = getPauseIcon();
+                        } else {
+                            audioPlayer.pause();
+                            playPauseButton.innerHTML = getPlayIcon();
+                        }
+                    }
                 });
 
                 function updatePlayer(track) {
                     document.querySelector('.player-cover').src = track.album.cover;
                     document.querySelector('.player-title').textContent = track.title;
                     document.querySelector('.player-artist').textContent = track.artist.name;
-                    audioPlayer.src = track.preview;  // Imposta l'anteprima audio come sorgente
-                    audioPlayer.play();  // Riproduce l'audio
+                    audioPlayer.src = track.preview;
+                    audioPlayer.play();
+                    playPauseButton.innerHTML = getPauseIcon();
                     document.querySelector('footer').style.display = 'flex';
+                    updateProgressBar();  // Inizia a aggiornare la barra di progresso
+                }
+
+
+                function updateProgressBar() {
+                    var progressBar = document.getElementById('progressBarSong');
+                    var currentTimeLabel = document.getElementById('current-time');
+                    var totalTimeLabel = document.getElementById('total-time');
+                    progressBar.value = 0;
+                    var duration = 30; // Durata in secondi della preview
+                    totalTimeLabel.textContent = "0:30"; // Imposta la durata totale
+
+                    var interval = setInterval(function() {
+                        if (audioPlayer.paused || audioPlayer.currentTime >= duration) {
+                            clearInterval(interval);
+                        } else {
+                            progressBar.value = audioPlayer.currentTime;
+                            currentTimeLabel.textContent = formatTime(audioPlayer.currentTime);
+                        }
+                    }, 1000);
+                }
+
+                function formatTime(seconds) {
+                    var minutes = Math.floor(seconds / 60);
+                    var seconds = Math.floor(seconds % 60);
+                    return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+                }
+
+
+
+                function getPlayIcon() {
+                    return '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-play-fill" viewBox="2 2 12 12">'+
+                    '<path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>'+
+                  '</svg>';
+                }
+
+                function getPauseIcon() {
+                    return '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pause-fill" viewBox="2 2 12 12">'+
+                    '<path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>'+
+                  '</svg>';
                 }
 
             } else {
@@ -107,4 +155,5 @@ hideActivitiesBtn.addEventListener("click", () => {
 showActivitiesBtn.addEventListener("click", () => {
   activities.classList.toggle("d-lg-block");
 });
+
 
